4674041e911d6206c88db314b824afa610604d2a
ZhouLan <1832311982@qq.com>
2019-08-25 15:14:49 +0800

    删除字符串空格

--- 0825/0825/test.c
+++ 0825/0825/test.c
@@ -22,11 +22,43 @@ int IsPowTow2(int n){
 	else return 0;
 }
 
+//删除字符串的空格 首尾删除 中间留一个空格 
+void DelBlank(char *str){
+	int i = 0;
+	int j = 0;
+	int flg = 0; //未处理
+	while (str[i] != '\0'){
+		if (str[i] != ' ' && !flg){
+			str[j++] = str[i++];
+			flg = 1;
+		}
+		else if (str[i] != ' ' && flg){
+			str[j++] = str[i++];
+		}
+		else if (str[i] == ' '&& !flg){
+			i++;
+		}
+		else if (str[i] == ' ' && flg){
+			str[j++] = str[i++];
+			flg = 0;
+		}
+	}
+	if (str[j-1] == ' '){
+		str[j-1] = '\0';
+	}
+	else str[j] = '\0';
+}
+
+//字符串压缩
+
 int main(){
 	printf("%d\n", IsPowTow(32));
 	printf("%d\n", IsPowTow(31));
 	printf("%d\n", IsPowTow2(32)); 
 	printf("%d\n", IsPowTow2(31)); 
+	char string[] = "   asd  rf g     d  ";
+	DelBlank(string);
+	printf("%s\n", string);
 	system("pause");
 	return 0;
 }
